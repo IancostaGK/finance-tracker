16790cf3a8d4f9dc7f0e2d402866284f35819fde
Ian Santos <ian.santos@leste.com>
2024-05-13 13:39:17 -0300

    feat: delete transaction

--- components/transaction.vue
+++ components/transaction.vue
@@ -4,8 +4,36 @@ import type { TransactionRow } from '~/types/Transaction';
 const props = defineProps<{
     transaction: TransactionRow;
 }>();
+const emit = defineEmits(['deleted']);
 
+const isLoading = ref(false);
+
+const toast = useToast();
+const supabase = useSupabaseClient();
 const { currency } = useCurrency(props.transaction.amount);
+
+const deleteTransaction = async () => {
+    isLoading.value = true;
+    try {
+        await supabase
+            .from('transactions')
+            .delete()
+            .eq('id', props.transaction.id);
+        toast.add({
+            title: 'Transaction deleted',
+            icon: 'i-heroicons-check-circle',
+        });
+        emit('deleted', props.transaction.id);
+    } catch (error) {
+        toast.add({
+            title: 'Transaction deleted',
+            icon: 'i-heroicons-exclamation-circle',
+        });
+    } finally {
+        isLoading.value = false;
+    }
+};
+
 const items = [
     [
         {
@@ -16,7 +44,7 @@ const items = [
         {
             label: 'Delete',
             icon: 'i-heroicons-trash-20-solid',
-            click: () => console.log('Delete'),
+            click: deleteTransaction,
         },
     ],
 ];
@@ -57,6 +85,7 @@ const icon = computed(() =>
                         color="white"
                         variant="ghost"
                         trailing-icon="i-heroicons-ellipsis-horizontal"
+                        :loading="isLoading"
                     />
                 </UDropdown>
             </div>
