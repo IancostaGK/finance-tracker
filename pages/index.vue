3c93a6c47af80d939461fb6b4bc36cf5f75ae66d
Ian Santos <ian.santos@leste.com>
2024-05-13 14:04:39 -0300

    feat: modal component

--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,7 @@ const supabase = useSupabaseClient();
 const transactions = ref<TransactionRow[]>([]);
 const viewSelected = ref(options[0]);
 const isLoading = ref(false);
+const isOpen = ref(false);
 
 const fetchTransactions = async (): Promise<TransactionRow[]> => {
     isLoading.value = true;
@@ -25,6 +26,15 @@ const fetchTransactions = async (): Promise<TransactionRow[]> => {
         isLoading.value = false;
     }
 };
+
+const income = computed(() =>
+    transactions.value.filter((t) => t.type === 'income')
+);
+const expense = computed(() =>
+    transactions.value.filter((t) => t.type === 'expense')
+);
+const incomeCount = computed(() => income.value.length);
+const expenseCount = computed(() => expense.value.length);
 
 const transactionsGroupedByDate = computed(() => {
     if (!transactions.value) return {};
@@ -48,66 +58,95 @@ await refreshTransactions();
 </script>
 
 <template>
-    <section class="flex items-center justify-between mb-10">
-        <h1 class="text-4xl font-extrabold">Summary</h1>
+    <div>
+        <section class="flex items-center justify-between mb-10">
+            <h1 class="text-4xl font-extrabold">Summary</h1>
 
-        <div>
-            <USelectMenu :options v-model="viewSelected" />
-        </div>
-    </section>
+            <div>
+                <USelectMenu :options v-model="viewSelected" />
+            </div>
+        </section>
 
-    <section
-        class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-16 mb-10"
-    >
-        <Trend
-            color="green"
-            title="Income"
-            :amount="4000"
-            :last-amount="3000"
-            :loading="isLoading"
-        />
-        <Trend
-            color="red"
-            title="Expense"
-            :amount="4000"
-            :last-amount="5000"
-            :loading="isLoading"
-        />
-        <Trend
-            color="green"
-            title="Investments"
-            :amount="4000"
-            :last-amount="3000"
-            :loading="isLoading"
-        />
-        <Trend
-            color="red"
-            title="Saving"
-            :amount="4000"
-            :last-amount="4100"
-            :loading="isLoading"
-        />
-    </section>
-
-    <section v-if="!isLoading">
-        <div
-            v-for="(transactionsOnDay, date) in transactionsGroupedByDate"
-            :key="date"
-            class="mb-10"
+        <section
+            class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-16 mb-10"
         >
-            <DailyTransactionsSummary
-                :date="date"
-                :transactions="transactionsOnDay"
+            <Trend
+                color="green"
+                title="Income"
+                :amount="income.length"
+                :last-amount="4100"
+                :loading="isLoading"
             />
-            <Transaction
-                v-for="transaction in transactionsOnDay"
-                :key="transaction.id"
-                :transaction="transaction"
-                @deleted="refreshTransactions()"
+            <Trend
+                color="red"
+                title="Expense"
+                :amount="expense.length"
+                :last-amount="3800"
+                :loading="isLoading"
             />
-        </div>
-    </section>
-    <section v-else>
-        <USkeleton class="h-8 w-full mb-2" v-for="i in 4" :key="i" />
-    </section>
+            <Trend
+                color="green"
+                title="Investments"
+                :amount="4000"
+                :last-amount="3000"
+                :loading="isLoading"
+            />
+            <Trend
+                color="red"
+                title="Saving"
+                :amount="4000"
+                :last-amount="4100"
+                :loading="isLoading"
+            />
+        </section>
+
+        <section class="flex justify-between mb-10">
+            <div>
+                <h2 class="text-2xl font-extrabold">Transactions</h2>
+                <div class="text-gray-500 dark:text-gray-400">
+                    You have {{ incomeCount }} incomes and
+                    {{ expenseCount }} expenses this period
+                </div>
+            </div>
+            <div>
+                <UButton
+                    icon="i-heroicons-plus-circle"
+                    color="white"
+                    variant="solid"
+                    label="Add"
+                    @click="isOpen = true"
+                />
+            </div>
+        </section>
+
+        <section v-if="!isLoading">
+            <div
+                v-for="(transactionsOnDay, date) in transactionsGroupedByDate"
+                :key="date"
+                class="mb-10"
+            >
+                <DailyTransactionsSummary
+                    :date="date"
+                    :transactions="transactionsOnDay"
+                />
+                <Transaction
+                    v-for="transaction in transactionsOnDay"
+                    :key="transaction.id"
+                    :transaction="transaction"
+                    @deleted="refreshTransactions()"
+                />
+            </div>
+        </section>
+        <section v-else>
+            <USkeleton class="h-8 w-full mb-2" v-for="i in 4" :key="i" />
+        </section>
+
+        <UModal v-model="isOpen">
+            <UCard>
+                <template #header> Add Transaction </template>
+
+                <div>Hello!</div>
+            </UCard>
+        </UModal>
+    </div>
 </template>
